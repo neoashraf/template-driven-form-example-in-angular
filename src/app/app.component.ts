481a63e1c678e92cef80c8293f1db2f5234fe066
neo <ashrafuddin160@gmail.com>
2018-04-21 15:29:22 +0600

    created a template-driven for m in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  categories = [
+    {id:"1",categoryName:"Computer Science"},
+    {id:"2",categoryName:"Arts"},
+    {id:"3",categoryName:"Finance"}
+  ]
+
+  submitted : boolean;
+
+  submit(submittedValues : NgForm){
+    this.submitted = true;
+    console.log(submittedValues.value);
+    console.log(submittedValues.valid);
+
+  }
+
 }
